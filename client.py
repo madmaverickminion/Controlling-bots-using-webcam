9c82f3cc6b96d37b6214bd2535a65dc253fc9d78
Saksham <ultron0603@gmail.com>
2021-08-03 16:20:44 +0530

    Client updated

--- client.py
+++ client.py
@@ -1,23 +1,17 @@
-
-# def object_tracking():
-    
-
-
-
-
-
-
-
 import socket
 import select
 import errno
 import sys
+import cv2 as cv
+import numpy as np
+import requests
+# global message
 HEADER_LENGTH=10
 
 IP="192.168.43.243"
 PORT=5050
 
-my_username=input("Username: ")
+my_username="head"
 
 client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -27,45 +21,59 @@ client_socket.setblocking(False)
 username=my_username.encode("utf-8")
 username_header=f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
 client_socket.send(username_header + username)
+#################################################################################################################
+#ip webcam live streaming link
+url='http://192.168.43.1:8080/shot.jpg'
+image_browser=requests.get(url)
+img_arr=np.array(bytearray(image_browser.content), dtype=np.uint8)
+img1=cv.imdecode(img_arr,-1)
+frame1=cv.resize(img1,(int(img1.shape[1]*0.55),int(img1.shape[0]*0.55)),interpolation=cv.INTER_AREA)
+image_browser=requests.get(url)
+img_arr=np.array(bytearray(image_browser.content), dtype=np.uint8)
+img2=cv.imdecode(img_arr,-1)
+frame2=cv.resize(img2,(int(img2.shape[1]*0.55),int(img2.shape[0]*0.55)),interpolation=cv.INTER_AREA)
+
+
 
 while True:
-    message=input(f"{my_username} > ")
-    # break_message=input()
-    # if break_message=="stop":
-    #     break
-    # message="ABC"
+    # message=input(f"{my_username} > ")
+    diff=cv.absdiff(frame1,frame2)
+    gray=cv.cvtColor(diff,cv.COLOR_BGR2GRAY)
+    blur=cv.GaussianBlur(gray,(7,7),1)
+    _,thres=cv.threshold(blur,20,255,cv.THRESH_BINARY)
+    dilated=cv.dilate(thres,None,iterations=3)
     
 
-    if message:
-        # message.encode("utf-8")
-        message_header=f"{len(message) :< {HEADER_LENGTH}}"
-        # message_header.encode("utf-8")
-        final_message=(message_header+message).encode("utf-8")
+    contours,_=cv.findContours(dilated,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 
-        client_socket.send(final_message)
+    for contour in contours:
+        (x,y,w,h)=cv.boundingRect(contour)
+        if cv.contourArea(contour)<1300:
+            continue
+        cv.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
+#############################################################################################        
+        #Main Algorithm for robot control(TO BE CONTINUED.......)
+        if((x,y)==(450,405)):
 
-    try:
-        while True:
-            #receive things
-            username_header=client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print("Connection closed by the server")
-                sys.exit()
-            username_length=int(username_header.decode("utf-8").strip())
-            username=client_socket.recv(username_length).decode("utf-8")
+            #check count of each and return accordingly
+            message = "MOVE LEFT"
+            message_header=f"{len(message) :< {HEADER_LENGTH}}"
+            # message_header.encode("utf-8")
+            final_message=(message_header+message).encode("utf-8")
 
-            message_header=client_socket.recv(HEADER_LENGTH)
-            message_length=int(message_header.decode("utf-8").strip())
-            message=client_socket.recv(message_length).decode("utf-8")
+            client_socket.send(final_message)
+#############################################################################################
+    cv.imshow('Live Stream',frame1)
+    frame1=frame2
+    
+    image_browser=requests.get(url)
+    img_arr=np.array(bytearray(image_browser.content), dtype=np.uint8)
+    img=cv.imdecode(img_arr,-1)
+    frame2=cv.resize(img,(int(img.shape[1]*0.55),int(img.shape[0]*0.55)),interpolation=cv.INTER_AREA)
 
-            print(f"{username} > {message}")
-    except IOError as e:
-        if e.errno!=errno.EAGAIN and e.errno!=errno.EWOULDBLOCK:
-            print('Reading error',str(e))
-            sys.exit()
 
-        continue
-    except Exception as e:
-        print('General error ',str(e))
-        sys.exit()
-        
+    if cv.waitKey(40) & 0xFF==ord('d'):
+        break
+
+
+    
