ab6aa6f75b581715e896fa1dbcd2ffc925505793
Saksham <ultron0603@gmail.com>
2021-08-09 00:24:52 +0530

    ______

--- boot.py
+++ boot.py
@@ -6,7 +6,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('realme narzo 20A', '579f15d38462')
+        sta_if.connect('', '')
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
